31bc51f7a5a91b3f1c155c011080c8869486e1c9
sennyoku <apple526283@docomo.ne.jp>
2023-09-18 21:44:05 +0900

    My Django Girls app, first commit

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,6 @@ from .forms import PostForm
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
@@ -19,14 +17,14 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit = False)
+            post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail',pk = post.pk)
-        else:
-            form = PostForm()
-    return render(request, 'blog/post_edit.html',{'form':form})
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
